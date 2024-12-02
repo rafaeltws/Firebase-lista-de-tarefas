17430ea0200caf323fefcc6dd2c659cbea8c4f74
rafaeltws <159142263+rafaeltws@users.noreply.github.com>
2024-12-02 16:25:26 -0300

    Update firebaseConn.js

--- src/firebase/firebaseConn.js
+++ src/firebase/firebaseConn.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOvZnnwHxuGFKPbAcgZDxhPX9bFZ7vnGI",
-  authDomain: "cursomatheusfraga.firebaseapp.com",
-  projectId: "cursomatheusfraga",
-  storageBucket: "cursomatheusfraga.firebasestorage.app",
-  messagingSenderId: "507429172888",
-  appId: "1:507429172888:web:af3c194fcd75b7f909c2ed",
-  measurementId: "G-JP23GHNXNE"
+  apiKey: //,
+  authDomain: //,
+  projectId: //,
+  storageBucket: //,
+  messagingSenderId: //,
+  appId: //,
+  measurementId: //
 };
 
   const firebaseApp = initializeApp(firebaseConfig);
